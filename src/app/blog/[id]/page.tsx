e65991cdbddb66e046ab84aa8ddfc65fb1efdb1f
刘宋健 <liusongjian@cyou-inc.com>
2024-12-06 19:05:50 +0800

    动态 metadata

--- src/app/blog/[id]/page.tsx
+++ src/app/blog/[id]/page.tsx
@@ -1,13 +1,21 @@
 import { Card } from 'antd'
 import { cardList } from '@/data'
 
-type Porps = {
+type Props = {
   params: {
+    title: string
     id: string
   }
 }
 
-export default function Page({ params }: Porps) {
+// 动态 metadata
+export async function generateMetadata({ params }: Props) {
+  return {
+    title: `博客详情 - ${params.title}`,
+  }
+}
+
+export default function Page({ params }: Props) {
   const cardInfo = cardList.find((item) => item.id === params.id)! // 添加非空断言
 
   return (
