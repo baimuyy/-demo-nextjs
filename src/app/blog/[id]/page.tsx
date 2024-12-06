0b7a13a34a7366869a73a4b6aaf9238132fee238
刘宋健 <liusongjian@cyou-inc.com>
2024-12-06 19:37:00 +0800

    动态路由，获取参数，打包报错问题

--- src/app/blog/[id]/page.tsx
+++ src/app/blog/[id]/page.tsx
@@ -1,23 +1,24 @@
+'use client'
+
 import { Card } from 'antd'
 import { cardList } from '@/data'
 
-type Props = {
-  params: {
+interface PageProps {
+  params: Promise<{
     id: string
-  }
+  }>
 }
 
 // 动态 metadata
-export async function generateMetadata({ params }: Props) {
-  const cardInfo = cardList.find((item) => item.id === params.id)! // 添加非空断言
-
+export async function generateMetadata({ params }: PageProps) {
+  const id = (await params).id
   return {
-    title: `博客详情 - ${cardInfo.title}`,
+    title: `博客详情 - ${id}`,
   }
 }
 
-export default function Page({ params }: Props) {
-  const cardInfo = cardList.find((item) => item.id === params.id)! // 添加非空断言
+export default function Page({ params }: PageProps) {
+  const cardInfo = cardList.find(async (item) => item.id === (await params).id)! // 添加非空断言
 
   return (
     <Card title={cardInfo.title}>
