40a608f80c9583082eeef5b25543f62163e7c450
刘宋健 <liusongjian@cyou-inc.com>
2024-12-06 19:09:51 +0800

    打包报错问题

--- src/app/blog/[id]/page.tsx
+++ src/app/blog/[id]/page.tsx
@@ -3,15 +3,16 @@ import { cardList } from '@/data'
 
 type Props = {
   params: {
-    title: string
     id: string
   }
 }
 
 // 动态 metadata
 export async function generateMetadata({ params }: Props) {
+  const cardInfo = cardList.find((item) => item.id === params.id)! // 添加非空断言
+
   return {
-    title: `博客详情 - ${params.title}`,
+    title: `博客详情 - ${cardInfo.title}`,
   }
 }
 
